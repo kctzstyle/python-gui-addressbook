2bb611226c999d779ab482f423126497621aa93e
kctzstyle <kctzstyle@gmail.com>
2021-01-30 18:04:21 +0900

    Update

--- addressbook/events.py
+++ addressbook/events.py
@@ -1,5 +1,22 @@
 
+from abc import ABCMeta
 
-class EventHandler:
-    pass
+
+class EventHandler(metaclass=ABCMeta):
+    
+    @staticmethod
+    def on_click(**kwargs):
+        pass
+    
+    @staticmethod
+    def on_message(**kwargs):
+        pass
+
+
+class AddressBookEventHandler(EventHandler):
+
+    @staticmethod
+    def on_click(**kwargs):
+        data = kwargs.get('data')
+        return data
 
